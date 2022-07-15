dd0067cd35cbd603186638e5bd79a967cf4a8ee9
Matthew <greenchicken1902@gmail.com>
2022-07-15 17:13:58 +1000

    Added the Miscellaneous files to be ignored.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BakkesMod-Plugin-Updator',
+    version='2.3.2',
+    packages=[''],
+    url='https://github.com/GreenMachine582/BakkesMod-Plugin-Updator',
+    license='MIT',
+    author='Matthew',
+    author_email='greenchicken1902@gmail.com',
+    description='Downloads and Installs plugins for BakkesMod'
+)
